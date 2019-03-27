bd03a31363f8d6a53696f1603e82b0be62d16495
Jacob Spade <spade.jacob@Yahoo.com>
2019-03-27 10:27:21 -0700

    setting up ejs

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,23 @@
 const router = require('express').Router();
 const Todo = require('../models/todo');
+const ejs = require('ejs');
 
 router.get('/', (req, res) => {
+  Todo.find({}).then((results) => {
+    res.render('index', {todos: results})
+  })
   res.render('index', {});
 });
 
 router.post('/todos', (req, res) => {
-  res.json(req.body);
+  let newTodo = new Todo({description: req.body.description});
+
+  newTodo
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.redirect('/')
+    })
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const routes = require('./routes/routes');
 
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost:27017/mongoose_express_todos', {
+  useMongoClient: true
+}).then(() => {
+  console.log('database connected...')
+})
+
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
